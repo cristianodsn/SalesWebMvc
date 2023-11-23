b7758fc39ff6fd139c4be763e53b0ff07ef4f166
Cristiano Castiel <cristianoct03@gmail.com>
2023-11-22 21:55:48 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,12 +1,34 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Value { get; set; } 
+        public double Value { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department() { }
 
+        public Department(int id, string name, double value)
+        {
+            Id = id;
+            Name = name;
+            Value = value;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(initial, final));
+        }
     }
 }
